93fe4124ff606fa29e60cf90a188f52163993b1f
Miodec <jack@monkeytype.com>
2023-12-11 22:14:00 +0100

    update base

--- days/base.ts
+++ days/base.ts
@@ -3,25 +3,30 @@ import * as Core from "../core";
 console.log("Running day {{day}}...");
 
 console.log("Getting input...");
-const inputLines: string[] = Core.loadFileAsArray("./input/{{day}}.txt");
+const inputStringData: string = Core.loadFileAsString("./input/{{day}}.txt");
 
 console.log("Getting example...");
-let exampleLines: string[] | null = null;
+let exampleStringData: string | null = null;
 if (Core.fileExists("./input/{{day}}_ex.txt")) {
-  exampleLines = Core.loadFileAsArray("./input/{{day}}_ex.txt") ?? [];
+  exampleStringData = Core.loadFileAsString("./input/{{day}}_ex.txt") ?? [];
 } else {
   console.error("Example input not found");
 }
 
-function parse(line: string, index: number) {
-  return;
+function parseData(dataString: string) {
+  return dataString.split("\n").map(parseLine);
+}
+
+function parseLine(line: string, index: number) {
+  return line;
 }
 
 console.log("Parsing input...");
-const inputData = inputLines.map(parse);
+const inputData = parseData(inputStringData);
 
-if (exampleLines !== null) console.log("Parsing example...");
-const exampleData = exampleLines !== null ? exampleLines!.map(parse) : null;
+if (exampleStringData !== null) console.log("Parsing example...");
+const exampleData =
+  exampleStringData !== null ? parseData(exampleStringData) : null;
 
 //
 // example targets
@@ -66,7 +71,7 @@ const onlyRunExamples = false;
 const p1 = onlyRunExamples ? undefined : part1();
 const p2 = onlyRunExamples ? undefined : part2();
 
-if (exampleLines === null) {
+if (exampleStringData === null) {
   console.error("Example data missing, can't check targets");
 } else {
   const p1ex = part1(true);
